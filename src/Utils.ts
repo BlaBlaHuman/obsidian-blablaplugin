61bcf0c7136c6ac96c093c76bd6e918ef732dcff
BlaBlaHuman <thelastrager29@gmail.com>
2023-09-16 15:03:20 +0200

    Add custom inline format support for {{date}} macro

--- src/Utils.ts
+++ src/Utils.ts
@@ -14,14 +14,19 @@ export async function getExpandedTemplate(template: TFile, plugin: BlaBlaPlugin)
     const moment = (<any>window).moment;
     const text = (await plugin.app.vault.read(template))
 				.replace(/{{date}}/gi, moment().format(plugin.settings.dateFormat))
+                .replace(/{{date\s*\:\s*(.+)}}/gi, (match, format) => {
+                    return moment().format(format);
+                })
 				.replace(/{{date\s*\+\s*(\d+)}}/gi, (match, number) => {
 					const daysToAdd = parseInt(number, 10);
 					const newDate = moment().add(daysToAdd, "days").format(plugin.settings.dateFormat);
-					return newDate;})
+					return newDate;
+                })
 				.replace(/{{date\s*-\s*(\d+)}}/gi, (match, number) => {
 					const daysToAdd = parseInt(number, 10);
 					const newDate = moment().add(-daysToAdd, "days").format(plugin.settings.dateFormat);
-					return newDate;})
+					return newDate;
+                })
 				.replace(/{{time}}/gi, moment().format(plugin.settings.timeFormat))
 				.replace(/{{title}}/gi, template.basename);
     return text;
